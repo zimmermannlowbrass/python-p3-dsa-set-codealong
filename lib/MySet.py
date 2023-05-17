43bef800cdfa776ff2355b75ea7968e052e00879
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-05-17 15:06:05 -0500

    completed labwork

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,21 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        if value not in self.dictionary:
+            self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
